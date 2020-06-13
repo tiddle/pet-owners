f84a5d2ee497c6b36da9a53b20b963f23eb1d6c2
Carlo Cruz <me@carlocruz.com>
2020-06-13 21:14:04 +1000

    Add tests and finish funcitonality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-export async function getData() {
+// Note: I would generally put these in separat JS files, but this project is so small, it would be easier to read the code if it were in 1 file.
 
+/**
+ * Retrieve data from API 
+ */
+export async function getData() {
 	// This should point to the live json, but I didn't want to have to create a server just to avoid CORS issues
-	const peopleData = await fetch('/people.json');
+	const peopleData = await fetch('/people.json').catch(err => console.log(err));
 	const peopleJson = await peopleData.json();
 
 	return peopleJson;
 }
 
+/**
+ * Denormalise the peopleArr so each pet has a dedicated owner
+ * 
+ * @param {Array} peopleArr 
+ */
 export function organisePeopleForPets(peopleArr) {
 	if (peopleArr && peopleArr.length > 0 && Array.isArray(peopleArr)) {
 		return peopleArr.reduce((acc, curr) => {
@@ -34,6 +43,12 @@ export function organisePeopleForPets(peopleArr) {
 	return [];
 }
 
+/**
+ * Remove all pets except for `type`
+ * 
+ * @param {Array} petArr 
+ * @param {String} type 
+ */
 export function filterPetsByType(petArr, type = 'Cat') {
 	if (petArr && petArr.length > 0 && Array.isArray(petArr)) {
 
@@ -45,11 +60,36 @@ export function filterPetsByType(petArr, type = 'Cat') {
 	return [];
 }
 
+/**
+ * Organise petArr into an object sorted by every unique `sort`
+ * 
+ * @param {Array} petArr 
+ * @param {String} sort 
+ */
 export function sortByOwner(petArr, sort = 'gender') {
-	
+	if (petArr && petArr.length > 0 && Array.isArray(petArr)) {
+		return petArr.reduce((acc, curr) => {
+			if(curr.owner && curr.owner[sort]) {
+				if(!acc[curr.owner[sort]]) {
+					acc[curr.owner[sort]] = {
+						sortName: curr.owner[sort],
+						pets: []
+					};
+				}
+
+				acc[curr.owner[sort]].pets.push(curr);
+			}
+
+
+			return acc;
+		}, {});
+	}
+
+	return {};
 }
 
 getData()
 	.then((people) => organisePeopleForPets(people))
 	.then((pets) => filterPetsByType(pets, 'Cat'))
+	.then((pets) => sortByOwner(pets, 'gender'))
 	.then((arr) => console.log(arr));
--- src/index.test.js
+++ src/index.test.js
@@ -1,13 +1,46 @@
-import { organisePeopleForPets } from './index';
+import { organisePeopleForPets, filterPetsByType } from './index';
 
-describe('test for organisePeopleForPets', () => {
-	test('empty param test', () => {
-    expect(organisePeopleForPets()).toEqual([])
-    expect(organisePeopleForPets([])).toEqual([])
-  });
-  
-  test('invalid param test', () => {
-    expect(organisePeopleForPets('aaaaa')).toEqual([]);
-    expect(organisePeopleForPets(123)).toEqual([]);
-  })
+describe('tests for organisePeopleForPets', () => {
+	test('when empty param', () => {
+		expect(organisePeopleForPets()).toEqual([]);
+		expect(organisePeopleForPets([])).toEqual([]);
+	});
+
+	test('when invalid param', () => {
+		expect(organisePeopleForPets('aaaaa')).toEqual([]);
+		expect(organisePeopleForPets(123)).toEqual([]);
+	});
+
+	describe('to organise the data', () => {
+		const data = [
+			{
+				name: 'Bob',
+				gender: 'Male',
+				age: 23,
+				pets: [
+					{ name: 'Garfield1', type: 'Cat' },
+					{ name: 'Garfield2', type: 'Cat' },
+				],
+			},
+		];
+
+		test('organise data', () => {
+			const result = organisePeopleForPets(data);
+			expect(result.length).toBe(2);
+      expect(result[0].name).toBe('Garfield1');
+      expect(result[0].owner.name).toBe('Bob');
+		});
+	});
+});
+
+describe('tests for filterPetsByType', () => {
+	test('when empty param', () => {
+		expect(filterPetsByType()).toEqual([]);
+		expect(filterPetsByType([])).toEqual([]);
+	});
+
+	test('when invalid param', () => {
+		expect(filterPetsByType('aaaaa')).toEqual([]);
+		expect(filterPetsByType(123)).toEqual([]);
+	});
 });
